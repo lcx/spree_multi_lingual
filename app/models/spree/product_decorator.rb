8c0369c9cadc1fb169fe2db8593d0ecca242f5b3
Cristian Livadaru <cristian@lcx.at>
2012-09-14 18:55:34 +0200

    removed permalink translation, bad idea for our shop

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,3 +1,3 @@
 Spree::Product.class_eval do
-  translates :name, :description, :meta_description, :meta_keywords, :permalink
+  translates :name, :description, :meta_description, :meta_keywords
 end
